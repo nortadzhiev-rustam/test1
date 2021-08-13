76dc96002c0c4ce48ecc186177f6d1a09e067120
Rustam Nortadzhiev <rustamnortojiyev@gmail.com>
2021-08-13 20:35:35 +0100

    master

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import axios from 'axios';
+import Todos from './components/todos';
+import { useDispatch } from 'react-redux';
+import { todosActions } from './store/todos-slice';
 function App() {
+  const [data, setData] = React.useState([]);
+  const [user, setUser] = React.useState([]);
+
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    axios
+      .get('http://jsonplaceholder.typicode.com/todos')
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => console.log('Error: ', err));
+
+    axios
+      .get('http://jsonplaceholder.typicode.com/users')
+      .then((res) => {
+        setUser(res.data);
+      })
+      .catch((err) => console.log('Error: ', err));
+  }, []);
+
+  React.useEffect(() => {
+    dispatch(
+      todosActions.todosAdded(
+        data.map((item) => ({
+          ...item,
+          ...user
+            .filter((usr) => usr.id === item.userId)
+            .map((usr) => {
+              return usr.name;
+            }),
+        }))
+      )
+    );
+  }, [dispatch, data, user]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <div className=''>
+        <Todos />
+      </div>
     </div>
   );
 }
